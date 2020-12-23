9720ae3f425cddac1de911df6a187b60aecaf8fb
EC2 Default User <ec2-user@ip-172-31-8-130.ec2.internal>
2020-12-23 10:04:25 +0000

    [Finish]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8166f0b3189dd82087867999ecd39df5ab5e0c938dac04c5fbe4545cb23136fa188d2daea546c3da38d4d4f8bd0269c3242c35f71bbd537f9990975eb4cc7ccc'
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => 'homes#show'
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update, :index, :create]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
 end
